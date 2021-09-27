e9e6b9795538b42208138403cc6ca944e8c94bdd
Iman Hussain <iman_hussain1@baylor.edu>
2021-09-27 15:44:52 -0500

    always

---
+++ sitka3.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import csv
+from datetime import datetime
+
+open_file= open("sitka_weather_2018_simple.csv","r")
+
+csv_file=csv.reader(open_file,delimiter=",")
+
+header_row=next(csv_file)
+
+print (type(header_row))
+
+for index,column_header in enumerate(header_row):
+    print(index,column_header)
+
+highs=[]
+
+lows=[]
+
+#testing the datetime strptime function
+mydate=datetime.strptime("2018-07-01","%Y-%m-%d")
+print(mydate)
+print(type(mydate))
+
+dates=[]
+
+for x in csv_file:
+    highs.append(int(x[5]))
+    lows.append(int(x[6]))
+    the_date=datetime.strptime(x[2], "%Y-%m-%d")
+    dates.append(the_date)
+
+print(dates)
+
+print(highs)
+
+print(lows)
+
+import matplotlib.pyplot as plt
+
+#autoformat the x axis that has dates to what you think is best
+
+fig=plt.figure()
+
+
+plt.title("Daily High Temperatures, July 2018",fontsize=16)
+plt.xlabel("",fontsize=12)
+plt.ylabel("Temperature (F)",fontsize=12)
+plt.tick_params(axis="both",labelsize=12)
+fig.autofmt_xdate()
+plt.plot(dates, highs,c="pink",alpha=0.5)
+plt.plot(dates,lows, c="purple",alpha=0.5)
+plt.fill_between(dates,highs,lows,facecolor="blue",alpha=0.1)
+
+plt.show()
+
